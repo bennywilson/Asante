76a9232ccad920a8f381772eac97cdd1f97fd885
Prescott Lawani <38223423+Lockeheede@users.noreply.github.com>
2023-06-17 18:16:36 -0500

    (Prescott) Laptop Update
    ->Started on ProjectileClass
    =>NEEDED<=
    ->Fix Spawn of TurretProjectile_BP

---
+++ Asante/Source/Asante/Public/AsanteProjectile.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "AsanteProjectile.generated.h"
+
+UCLASS()
+class ASANTE_API AAsanteProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AAsanteProjectile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	/** Sphere collision component */
+	UPROPERTY(VisibleDefaultsOnly, Category=Projectile)
+	USphereComponent* CollisionComp;
+
+	/** Projectile movement component */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	UProjectileMovementComponent* ProjectileMovement;
+
+	/** Static Mesh component */
+	UPROPERTY(Category = StaticMeshActor, VisibleAnywhere, BlueprintReadOnly, meta = (ExposeFunctionCategories = "Mesh,Rendering,Physics,Components|StaticMesh", AllowPrivateAccess = "true"))	
+		UStaticMeshComponent *StaticMesh;
+
+};
